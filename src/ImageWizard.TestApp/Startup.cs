ad8d4b62c9e0f75eceb4499ce484f4358d44bfd9
usercode <2958488+usercode@users.noreply.github.com>
2021-02-20 01:21:35 +0100

    Added libgdiplus and mscorefonts to dockerfile

--- src/ImageWizard.TestApp/Startup.cs
+++ src/ImageWizard.TestApp/Startup.cs
@@ -118,7 +118,7 @@ namespace ImageWizard.TestApp
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
@@ -129,7 +129,6 @@ namespace ImageWizard.TestApp
             {
                 x.MapRazorPages();
                 x.MapControllers();
-                //x.MapImageWizard();
             });
         }
     }
